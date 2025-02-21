f8aa8c83b25138cef30e80bcb2a1de3a561065b3
Petr Puš <pus@corsum.cz>
2025-02-21 14:48:02 +0100

    by PP

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Board from "./components/Board";
 function App() {
   return (
     <div className="w-full h-full flex flex-col gap-4 md:gap-8 justify-center items-center pt-4 px-2 sm:pt-8 sm:px-4 md:pt-12 md:px-8 lg:pt-16 lg:px-12">
-      <p className="text-4xl font-bold">Strands Solver</p>
+      <p className="text-4xl font-bold">Strands Solver v0</p>
+      <p>by Petr Puš</p>
       <Suspense fallback={<p>Loading words...</p>}>
         <Board />
       </Suspense>
